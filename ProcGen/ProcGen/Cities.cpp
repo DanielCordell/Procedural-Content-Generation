4ebbce09bb9446a7b09e3d10cb1a0ed717947451
Daniel Cordell <danielbc@hotmail.co.uk>
2017-12-30 00:30:27 +0000

    Added some TODOs

--- ProcGen/ProcGen/Cities.cpp
+++ ProcGen/ProcGen/Cities.cpp
@@ -11,12 +11,14 @@ bool CanHaveVillage(sf::Vertex position)
 
 std::vector<sf::Vector2i> getCityCoords(sf::Vector2i position) {
 	std::vector<sf::Vector2i> cityCoords;
+	//TODO: Implement size difference (small/med/large)
 	for (int x = -3; x <= 3; ++x) {
 		for (int y = -3; y <= 3 ; ++y) {
 			if (abs(x) + abs(y) <= 3) cityCoords.push_back({ position.x + x, position.y + y });
 		}
 	}
 
+	//TODO: City class
 
 	for (auto a : cityCoords) std::cout << a.x << " " << a.y << std::endl;
 	std::cout << "\n";
