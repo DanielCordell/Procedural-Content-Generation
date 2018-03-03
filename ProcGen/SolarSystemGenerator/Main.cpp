2f69f444b6b1a2363e751c36047ec8da471232c6
Daniel Cordell <danielbc@hotmail.co.uk>
2018-03-03 02:45:49 +0000

    Started a galaxy generator cuz I sort've wrote myself into a hole with the terrain generator. Gonna have to rewrite it all at some point, so i'll do this first to ease the monotony.

---
+++ ProcGen/SolarSystemGenerator/Main.cpp
@@ -0,0 +1,52 @@
+
+#include <cstdlib>
+#include <ctime>
+
+#include <SFML/Graphics.hpp>
+#include "Sizes.h"
+
+int main() {
+	srand(time(nullptr));
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Noise", sf::Style::Default, sf::ContextSettings{ 0,0,2 });
+	bool doQuit = false;
+
+	sf::Clock c;
+	sf::Time sinceLastTick = sf::Time::Zero;
+	while (true) {
+		if (sinceLastTick.asSeconds() < 1 / 60.f) {
+			sinceLastTick += c.restart();
+			continue;
+		}
+		if (doQuit) break;
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				doQuit = true;
+			case sf::Event::MouseButtonPressed:
+			case sf::Event::KeyPressed:
+				//if (*isGenerating) break;
+				if (event.key.code == sf::Keyboard::S) {
+					sf::RenderTexture tex;
+					tex.create(XMAX, YMAX);
+					tex.clear();
+					//tex.setView(view);
+					//tex.draw(grid);
+					tex.display();
+					tex.getTexture().copyToImage().saveToFile("../img.png");
+					break;
+				}
+				//*isGenerating = true;
+			}
+		}
+		window.clear(sf::Color::Black);
+		//window.draw(grid);
+		//window.draw(cities);
+		//window.draw(cityData);
+
+		window.display();
+	}
+	//thread.join();
+	window.close();
+}
---
+++ ProcGen/SolarSystemGenerator/Sizes.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define XMAX 1920.0
+#define YMAX 1080.0
